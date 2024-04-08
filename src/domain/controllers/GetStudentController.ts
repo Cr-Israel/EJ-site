16ebb72f26c37d59916f4b6842b748e3a65c101b
Cr-Israel <carlosisrael08@hotmail.com>
2024-04-08 15:55:20 -0300

    Get Student by ID

---
+++ src/domain/controllers/GetStudentController.ts
@@ -0,0 +1,21 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import database from "../../../in-memory-database/database"
+import { z } from "zod";
+
+export class GetStudentController {
+  async execute(request: FastifyRequest, reply: FastifyReply) {
+    const getStudentParams = z.object({
+      studentId: z.string()
+    })
+
+    const { studentId } = getStudentParams.parse(request.params)
+
+    const student = await database.find((student) => student.studentId === studentId)
+
+    if (!student) {
+      return reply.status(404).send({ messsage: 'Estudante não encontrado' });
+    }
+
+    reply.send(student)
+  }
+}
--- src/routes/get-student.ts
+++ src/routes/get-student.ts
@@ -1,21 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { z } from "zod";
-import database from "../../in-memory-database/database"
+import { GetStudentController } from "../domain/controllers/GetStudentController";
 
 export async function getStudent(app: FastifyInstance) {
-  app.get('/get-student/:studentId', async (request, reply) => {
-    const getStudentParams = z.object({
-      studentId: z.string()
-    })
-
-    const { studentId } = getStudentParams.parse(request.params)
-
-    const student = await database.find((student) => studentId === studentId)
-
-    if (!student) {
-      return reply.status(400).send({ messsage: 'Estudante não encontrado' });
-    }
-
-    reply.send(student)
-  })
+  app.get('/get-student/:studentId', new GetStudentController().execute)
 }
