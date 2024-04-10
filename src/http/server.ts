0b8c69ece206bc8b653f005a0281b3f2526f192d
Cr-Israel <carlosisrael08@hotmail.com>
2024-04-10 17:49:56 -0300

    feat: server and client http

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,11 @@
 import fastify from "fastify";
 import { getStudent } from "../routes/get-student";
-const app = fastify();
+import { createStudent } from "../routes/create-student";
+
+const app = fastify({ logger: true });
 
 app.register(getStudent)
+app.register(createStudent)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
