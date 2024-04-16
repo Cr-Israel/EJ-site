8d02be950d2aad390f47fda61276157e5238fcaf
Cr-Israel <carlosisrael08@hotmail.com>
2024-04-16 15:33:40 -0300

    feat: create route and controller to get all students in database

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
 import { getStudent } from "../routes/get-student";
 import { createStudent } from "../routes/create-student";
+import { getAllStudent } from "../routes/get-all-student";
 
 const app = fastify();
 
 app.register(getStudent)
+app.register(getAllStudent)
 app.register(createStudent)
 
 app.listen({ port: 3333 }).then(() => {
